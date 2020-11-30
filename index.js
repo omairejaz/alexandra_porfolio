bf529d7bb90f11014ec9d791346e3f42f9f02288
omairejaz <62966685+omairejaz@users.noreply.github.com>
2020-11-30 22:09:44 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+$("#b1").click(function () {
+    $(".div-b2").css("display","none")
+    $(".div-b1").css("display","block")
+    $("#b1").css("background-color"," #581b98")
+    $("#b1").css("color","white")
+    $("#b2").css("background-color"," #f1f1f1")
+    $("#b2").css("color","black")
+    $(".video-reel1").css("height","5210px")
+})
+
+$("#b2").click(function () {
+    $(".div-b1").css("display","none")
+    $(".div-b2").css("display","block")
+    $("#b2").css("background-color"," #581b98")
+    $("#b2").css("color","white")
+    $("#b1").css("background-color"," #f1f1f1")
+    $("#b1").css("color","black")
+    $(".video-reel1").css("height","5310px")
+})
+
+$("#bb1").click(function () {
+    $(".dp2").css("display","none")
+    $(".dp1").css("display","block")
+    $("#bb1").css("background-color"," #581b98")
+    $("#bb1").css("color","white")
+    $("#bb2").css("background-color"," #f1f1f1")
+    $("#bb2").css("color","black")
+    $(".video-reel1").css("height","7710px")
+})
+
+$("#bb2").click(function () {
+    $(".dp1").css("display","none")
+    $(".dp2").css("display","block")
+    $("#bb2").css("background-color"," #581b98")
+    $("#bb2").css("color","white")
+    $("#bb1").css("background-color"," #f1f1f1")
+    $("#bb1").css("color","black")
+    $(".video-reel1").css("height","4110px")
+})
+
